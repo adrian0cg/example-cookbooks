7cccdcb271f9fd290400ca26d1927d6ec55af525
Jonathan Weiss <jw@innerewut.de>
2011-07-07 17:35:51 +0200

    remove mounts example: does not work this way

--- mounts/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default[:elasticsearch][:data_dir] = '/var/lib/elasticsearch'
-set[:scalarium_initial_setup][:bind_mounts][:mounts] = scalarium_initial_setup[:bind_mounts][:mounts].update({
-  '/var/log/elasticsearch' => '/mnt/var/log/elasticsearch',
-  elasticsearch[:data_dir] => "/mnt#{elasticsearch[:data_dir]}"
-})
