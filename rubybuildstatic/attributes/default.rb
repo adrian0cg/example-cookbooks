501fcec4c312e6c5ae5a3c50beaf44c00024339f
Daniel Hüsch <daniel.huesch@gmail.com>
2011-10-12 08:41:09 +0200

    Modify rubybuild cookbook for static linking.

---
+++ rubybuildstatic/attributes/default.rb
@@ -0,0 +1,15 @@
+default[:rubybuild] = {}
+default[:rubybuild][:version] = '1.9.2'
+default[:rubybuild][:patch] = 'p290'
+default[:rubybuild][:basename] = "ruby-#{node[:rubybuild][:version]}-#{node[:rubybuild][:patch]}"
+default[:rubybuild][:pkgrelease] = '4'
+default[:rubybuild][:prefix] = '/root/scalarium-agent/vendor'
+default[:rubybuild][:configure] = '--disable-install-doc'
+default[:rubybuild][:arch] = node[:kernel][:machine] == 'x86_64' ? 'amd64' : 'i386'
+default[:rubybuild][:tbz2] = "ruby1.9-static_#{node[:rubybuild][:version]}-#{node[:rubybuild][:patch]}.#{node[:rubybuild][:pkgrelease]}_#{node[:rubybuild][:arch]}.tbz2"
+default[:rubybuild][:s3] = {}
+default[:rubybuild][:s3][:upload] = false
+default[:rubybuild][:s3][:bucket] = ''
+default[:rubybuild][:s3][:path] = "#{node[:platform]}/#{node[:platform_version]}"
+default[:rubybuild][:s3][:aws_access_key] = ""
+default[:rubybuild][:s3][:aws_secret_access_key] = ""
---
+++ rubybuildstatic/recipes/default.rb
@@ -0,0 +1,57 @@
+execute "apt-get update"
+
+package "s3cmd" do
+  only_if do
+    node[:rubybuild][:s3][:upload]
+  end
+end
+
+package "libffi-dev"
+package 'libreadline-dev'
+
+remote_file "/tmp/#{node[:rubybuild][:basename]}.tar.bz2" do
+  source "http://ftp.ruby-lang.org/pub/ruby/#{node[:rubybuild][:basename]}.tar.bz2"
+end
+
+execute "tar xvfj #{node[:rubybuild][:basename]}.tar.bz2" do
+  cwd "/tmp"
+end
+
+execute "./configure --prefix=#{node[:rubybuild][:prefix]} #{node[:rubybuild][:configure]}" do
+  cwd "/tmp/#{node[:rubybuild][:basename]}"
+end
+
+execute "sed -i 's/#option nodynamic/option nodynamic/' ext/Setup" do
+  cwd "/tmp/#{node[:rubybuild][:basename]}"
+end
+
+execute "make" do
+  cwd "/tmp/#{node[:rubybuild][:basename]}"
+end
+
+execute "make install" do
+  cwd "/tmp/#{node[:rubybuild][:basename]}"
+end
+
+execute "tar cfj #{node[:rubybuild][:tbz2]} #{node[:rubybuild][:prefix]}/ruby" do
+  cwd "/tmp/#{node[:rubybuild][:basename]}"
+end
+
+template "/tmp/.s3cfg" do
+  source "s3cfg.erb"
+  only_if do
+    node[:rubybuild][:s3][:upload]
+  end
+end
+
+execute "s3cmd -c /tmp/.s3cfg put --acl-public --guess-mime-type #{node[:rubybuild][:tbz2]} s3://#{node[:rubybuild][:s3][:bucket]}/#{node[:rubybuild][:s3][:path]}/" do
+  cwd "/tmp/#{node[:rubybuild][:basename]}"
+  only_if do
+    node[:rubybuild][:s3][:upload]
+  end
+end
+
+file "/tmp/.s3cfg" do
+  action :delete
+  backup false
+end
