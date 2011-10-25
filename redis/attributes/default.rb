b467be45eead14135780e4c98b575666e81818e5
Jonathan Weiss <jw@innerewut.de>
2011-10-25 08:14:25 +0200

    major update to the Redis cookbook

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,8 +1,8 @@
-default[:redis] = {}
 default[:redis][:bind_address] = '0.0.0.0'
 default[:redis][:port] = 6379
-default[:redis][:timeout] = 300
-default[:redis][:version] = '2.0.4'
+default[:redis][:timeout] = 0
+default[:redis][:version] = '2.4.1'
+default[:redis][:cli][:version] = redis[:version]
 default[:redis][:prefix] = '/usr/local'
 default[:redis][:user] = 'redis'
 default[:redis][:datadir] = '/var/lib/redis'
@@ -13,7 +13,21 @@ default[:redis][:dump_file] = 'dump.rdb'
 default[:redis][:appendonly] = 'yes'
 default[:redis][:aofile] = 'appendonly.aof'
 default[:redis][:appendfsync] = 'everysec'
-default[:redis][:vm] = 'yes'
-default[:redis][:vm_max_memory] = '385875968'
+default[:redis][:no_appendfsync_on_rewrite] = 'no'
+default[:redis][:auto_aof_rewrite_percentage] = '150'
+default[:redis][:auto_aof_rewrite_min_size] = '512mb'
+
+# depricated VM
+default[:redis][:vm] = 'no'
+default[:redis][:vm_max_memory] = '3558759680'
 default[:redis][:vm_page_size] = '32'
-default[:redis][:vm_pages] = '134217728'
+default[:redis][:vm_pages] = '268435456'
+default[:redis][:swapfile] = '/mnt/redis/redis.swap'
+
+# backup & compaction
+default[:redis][:compact_every_x_days] = "*/2" 
+default[:redis][:compact_at] = '3'
+default[:redis][:backup_hour] = "0-23/4"
+default[:redis][:backup_minute] = '30'
+default[:redis][:backupdir] = '/vol/backups/redis'
+default[:redis][:backup_backlog] = 10
--- redis/recipes/configure-client.rb
+++ redis/recipes/configure-client.rb
@@ -1,5 +1,3 @@
-include_recipe "deploy" # get the deployment attributes
-
 node[:deploy].each do |application, deploy|
   if deploy[:application_type] != 'rails'
     Chef::Log.debug("Skipping redis::configure as application #{application} as it is not an Rails app")
