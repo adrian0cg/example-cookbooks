e831b708fb5eff49233913bf2aa861e9171dec1c
Daniel Hüsch <daniel.huesch@gmail.com>
2011-12-06 17:21:08 +0100

    wip

--- rubybuild/recipes/default.rb
+++ rubybuild/recipes/default.rb
@@ -11,6 +11,15 @@ package "libffi-dev"
 package 'libreadline-dev'
 package 'libyaml-dev'
 
+def perform(cmd, dir = "/tmp/#{node[:rubybuild][:basename]}")
+  execute cmd do
+    cwd dir
+  end
+end
+
+perform('git clone git://github.com/sstephenson/ruby-build.git', '/tmp')
+perform('env PREFIX=/tmp ./install.sh', '/tmp/ruby-build')
+
 remote_file "/tmp/#{node[:rubybuild][:basename]}.tar.bz2" do
   source "http://ftp.ruby-lang.org/pub/ruby/1.9/#{node[:rubybuild][:basename]}.tar.bz2"
 end
@@ -19,13 +28,18 @@ execute "tar xvfj #{node[:rubybuild][:basename]}.tar.bz2" do
   cwd "/tmp"
 end
 
-execute "./configure --prefix=#{node[:rubybuild][:prefix]} #{node[:rubybuild][:configure]}" do
-  cwd "/tmp/#{node[:rubybuild][:basename]}"
-end
+perform "./configure --prefix=#{node[:rubybuild][:prefix]} #{node[:rubybuild][:configure]}"
+perform 'make all install'
+perform "checkinstall -y -D --pkgname=ruby1.9 --pkgversion=#{node[:rubybuild][:version]} --pkgrelease=#{node[:rubybuild][:patch]}.#{node[:rubybuild][:pkgrelease]} --maintainer=mathias.meyer@scalarium.com --pkggroup=ruby --pkglicense='Ruby License' --install=no make all install"
 
-execute "checkinstall -y -D --pkgname=ruby1.9 --pkgversion=#{node[:rubybuild][:version]} --pkgrelease=#{node[:rubybuild][:patch]}.#{node[:rubybuild][:pkgrelease]} --maintainer=mathias.meyer@scalarium.com --pkggroup=ruby --pkglicense='Ruby License' make all install" do
-  cwd "/tmp/#{node[:rubybuild][:basename]}"
-end
+perform('rm -rf /usr/local')
+perform('/tmp/bin/ruby-build 1.9.3-p0 /usr/local')
+
+perform("ar x #{node[:rubybuild][:deb]}")
+perform('tar xfz data.tar.gz')
+perform('cp -r /usr/local/* usr/local/')
+perform("tar cfz data.tar.gz usr/")
+perform("ar r #{node[:rubybuild][:deb]} debian-binary control.tar.gz data.tar.gz")
 
 template "/tmp/.s3cfg" do
   source "s3cfg.erb"
