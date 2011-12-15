b3e4f753e84f30032900e5ab63e4b3bb3030871d
Jonathan Weiss <jw@innerewut.de>
2011-12-15 17:09:18 +0100

    use build-in deploy-definition

--- sphinx/recipes/deploy.rb
+++ sphinx/recipes/deploy.rb
@@ -11,51 +11,9 @@ unless node[:scalarium][:instance][:roles].include?('rails-app')
       path deploy[:deploy_to]
     end
 
-    prepare_git_checkouts(:user => deploy[:user], 
-                        :group => deploy[:group], 
-                        :home => deploy[:home], 
-                        :ssh_key => deploy[:scm][:ssh_key]) if deploy[:scm][:scm_type].to_s == 'git'
-
-    %w(log config system pids).each do |dir_name|
-      directory "#{deploy[:deploy_to]}/shared/#{dir_name}" do
-        group deploy[:group]
-        owner deploy[:user]
-        mode "0770"
-        action :create
-        recursive true  
-      end
-    end
-
-    directory "#{deploy[:deploy_to]}/shared/cached-copy" do
-      recursive true
-      action :delete
-    end
-
-    deploy deploy[:deploy_to] do
-      repository deploy[:scm][:repository]
-      user deploy[:user]
-      revision deploy[:scm][:revision]
-      migrate false
-      environment "RAILS_ENV" => deploy[:rails_env], "RUBYOPT" => ""
-      symlink_before_migrate({})
-      restart_command "echo"
-      action deploy[:action]
-      case deploy[:scm][:scm_type].to_s
-      when 'git'
-        scm_provider Chef::Provider::Git
-        enable_submodules deploy[:enable_submodules]
-        shallow_clone true
-      when 'svn'
-        scm_provider Chef::Provider::Subversion
-        svn_username deploy[:scm][:user]
-        svn_password deploy[:scm][:password]
-      else
-        raise "unsupported SCM type #{deploy[:scm][:scm_type].inspect}"
-      end
-      before_migrate lambda{}
-      after_restart lambda{}
-      before_symlink lambda{}
-      before_restart lambda{}
+    scalarium_deploy do
+      deploy_data deploy
+      app application
     end
 
     template "#{deploy[:deploy_to]}/current/config/database.yml" do
