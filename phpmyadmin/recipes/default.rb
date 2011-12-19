c2b9692bfe3eb202ecf1878a60bf12b796197905
Thomas Metschke <thomas.metschke@gmail.com>
2011-12-19 13:07:20 +0100

    update and fix phpmyadmin example

--- phpmyadmin/recipes/default.rb
+++ phpmyadmin/recipes/default.rb
@@ -1,3 +1,5 @@
+db_master = node[:scalarium][:roles]['db-master'][:instances].keys.first
+
 directory "/etc/dbconfig-common/" do
   action :create
   recursive true
@@ -6,6 +8,7 @@ end
 template "/etc/dbconfig-common/phpmyadmin.conf" do
   source "phpmyadmin.conf.erb"
   mode "0660"
+  variables(:password => node[:mysql][:server_root_password], :username => 'root', :host => (node[:scalarium][:roles]['db-master'][:instances][db_master][:private_dns_name] rescue nil) )
 end
 
 package "phpmyadmin"
@@ -20,9 +23,9 @@ template "/etc/phpmyadmin/apache.conf" do
   mode "0660"
 end
 
-db_master = node[:scalarium][:roles]['db-master'][:instances].keys.first
 template "/etc/phpmyadmin/config-db.php" do
   source "config-db.php.erb"
+  owner 'www-data'
   mode "0660"
-  variables(:password => node[:mysql_server_root_password], :username => 'root', :host => (node[:scalarium][:roles][:redis][:instances][db_master][:private_dns_name] rescue nil) )
+  variables(:password => node[:mysql][:server_root_password], :username => 'root', :host => (node[:scalarium][:roles]['db-master'][:instances][db_master][:private_dns_name] rescue nil) )
 end
