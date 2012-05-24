0ead17322fcc4ba8d24ed6c0b172119ec8353f55
Jonathan Weiss <jw@innerewut.de>
2012-05-24 17:07:07 +0200

    fix gitosis for Ubuntu

--- gitosis/recipes/default.rb
+++ gitosis/recipes/default.rb
@@ -1,14 +1,18 @@
 package "git-core"
 package "gitosis"
 
-user "git" do
-  comment "Gitosis/Git user"
-  gid "users"
-  home "/srv/gitosis/"
-end
+#user "git" do
+#  comment "Gitosis/Git user"
+#  gid "users"
+#  home "/srv/gitosis/"
+#end
 
-directory "/srv/gitosis" do
-  action :create
-  owner "git"
-  group "users"
+#directory "/srv/gitosis" do
+#  action :create
+#  owner "git"
+#  group "users"
+#end
+
+execute "ensure correct permissions for gitosis" do
+  command "chown -R gitosis /srv/gitosis"
 end
