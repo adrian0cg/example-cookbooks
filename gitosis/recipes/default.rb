2cb198a4f7d03d0e3e61520799594c99658841ee
Jonathan Weiss <jw@innerewut.de>
2012-05-24 18:12:16 +0200

    rename gitosis user to git

--- gitosis/recipes/default.rb
+++ gitosis/recipes/default.rb
@@ -16,3 +16,7 @@ package "gitosis"
 execute "ensure correct permissions for gitosis" do
   command "chown -R gitosis /srv/gitosis"
 end
+
+execute "rename gitosis user to git" do
+  usermod -l git gitosis
+end
